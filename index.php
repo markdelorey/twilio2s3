b39af981ac73c15d2baa5e60f72ab12e9472df4b
Mark Delorey <mark.delorey@gmail.com>
2014-01-13 22:22:37 -0700

    Initial commit - Download/upload 10 files

---
+++ index.php
@@ -0,0 +1,47 @@
+<?php
+
+require 'vendor/autoload.php';
+require 'config.php';
+
+// Instantiate the twilio client
+$twilio	=	new Services_Twilio(TW_ACCOUNT_SID, TW_SECRET_KEY);
+
+use Aws\S3\S3Client;
+
+// Instantiate the S3 client with your AWS credentials
+$s3 = S3Client::factory(array(
+	'key'    => AWS_ACCESS_KEY,
+	'secret' => AWS_SECRET_KEY,
+));
+
+// Create an array to hold the recordings map
+$recordings	=	array();
+
+// Get all the audio recordings for an account
+if( count($twilio->account->recordings->getPage(0,10)) > 0 ) {
+
+	foreach( $twilio->account->recordings->getPage(0,10)->getIterator() as $r ) {
+	
+		// Map the recording SID to the existing audio URL
+		$recordings[]	=	array( 'sid' => $r->sid, 'old' => 'http://api.twilio.com'. $r->uri .'.mp3' );
+		
+		$filename	=	'audio/'. $r->sid .'.mp3';
+		
+		file_put_contents('audio/'. $r->sid .'.mp3', fopen("http://api.twilio.com". $r->uri, 'r'));
+		
+		// Upload a file.
+		$result = $s3->putObject(array(
+			'Bucket'       => S3_BUCKET_NAME,
+			'Key'          => $r->sid .'.mp3',
+			'SourceFile'   => $filename,
+			'ContentType'  => 'audio/mpeg',
+			'ACL'          => 'public-read',
+		));
+		
+		echo 'RESULT: '. $result;
+		
+	}
+	
+}
+
+?>
